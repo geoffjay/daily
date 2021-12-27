184e55c89ff19f79f1e24c52dc2688984b85a4ad
Geoff Johnson <geoff.jay@gmail.com>
2021-12-27 11:05:27 -0800

    feat: add rubocop

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  resources :users, only: %i[new create]
+
+  # authorization endpoints
+  scope module: '/auth' do
+    get '/register', to: 'registrations#new'
+    post '/register', to: 'registrations#create'
+
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  end
 end
